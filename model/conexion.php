cd651434b41bc3e7b627266500ddf93abd139621
david12may <107639802+david12may@users.noreply.github.com>
2022-06-16 11:58:04 -0500

    Add files via upload

---
+++ model/conexion.php
@@ -0,0 +1,17 @@
+<?php 
+$contrasena = "D@123456";
+$usuario = "user1utp";
+$nombre_bd = "crud";
+
+try {
+	$bd = new PDO (
+		'mysql:host=servis1.mysql.database.azure.com;
+		dbname='.$nombre_bd,
+		$usuario,
+		$contrasena,
+		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+	);
+} catch (Exception $e) {
+	echo "Problema con la conexion: ".$e->getMessage();
+}
+?>
